c7b23f1b0dc89153af7f3e66e6a0be3e230699ba
Sandor Szabo <sanyiszaboqt@gmail.com>
2024-06-03 22:41:56 -0500

    header fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./Header/Header";
 import selfie from "./assets/jerry.jpeg";
 import FirstProject from "./firstProject/FirstProject";
