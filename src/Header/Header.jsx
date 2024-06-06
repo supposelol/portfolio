f6131b37985d0f7b9f11ca20ef373db7b1cf4b9e
Sandor Szabo <sanyiszaboqt@gmail.com>
2024-06-06 07:08:31 -0500

    mobile header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSpring, animated } from "react-spring";
+import EmailLink from "../components/links/emailLink";
+import DiscordLink from "../components/links/discordLink";
 import logo from "../assets/Sandor_transparent.png";
 import "./header.css";
 import "./hamburger.css"
+import GHLink from "../components/links/githubLink";
+import LinkedinLink from "../components/links/linkedinLink";
 
 const Header = ({ goToSection }) => {
     const handleClick = (sectionId) => {
         goToSection(sectionId);
     };
+
+    const [openMenu, setOpenMenu] = useState(false);
+    const menuAnimation = useSpring({
+        opacity: openMenu ? 1 : 0,
+        config: { duration: 300 },
+    });
+
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpenMenu(false)
+            }
+        };
+
+        document.body.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.body.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
     return (
         <header>
             <div id="logo-container">
@@ -26,7 +54,10 @@ const Header = ({ goToSection }) => {
                     <p>Resume</p>
                 </button>
             </div>
-            <div className="hamburger-container">
+
+            <div className={`hamburger-container ${openMenu ? 'open' : ''}`}
+                onClick={() => setOpenMenu(!openMenu)}
+                ref={menuRef}>
                 <div class="background">
                     <button class="menu__icon">
                         <span></span>
@@ -35,6 +66,38 @@ const Header = ({ goToSection }) => {
                     </button>
                 </div>
             </div>
+
+            {openMenu && (
+                <animated.ul className="mobile-menu" style={menuAnimation}>
+                    <div className="mobile-nav-links">
+                        <div className="nav-item" onClick={() => handleClick('section2')}>
+                            <p className="darkpink">1.</p><p className="marginright eiderwhite nav-text">About</p>
+                        </div>
+                        <div className="nav-item" onClick={() => handleClick('section3')}>
+                            <p className="darkpink">2.</p><p className="marginright eiderwhite nav-text">Projects</p>
+                        </div>
+                        <div className="nav-item" onClick={() => handleClick('section5')}>
+                            <p className="darkpink">3.</p><p className="marginright eiderwhite nav-text">Contact</p>
+                        </div>
+                    </div>
+                    <div className="mobile-socials">
+                        <p className="lightgray">Say hello</p>
+                        <EmailLink email="szabo.shani@gmail.com">
+                            szabo.shani@gmail.com
+                        </EmailLink>
+                        <ul className="mobile-short-links">
+                            <li><LinkedinLink /></li>
+                            <li><GHLink /></li>
+                            <li><DiscordLink /></li>
+                        </ul>
+                    </div>
+                    <button className="button">
+                        <p>Resume</p>
+                    </button>
+
+                </animated.ul>
+            )}
+
         </header>
     );
 };
---
+++ src/components/links/githubLink.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GHLink() {
+    const profileURL = `https://github.com/supposelol/`;
+
+    return (
+        <a className="contacts-link eiderwhite" href={profileURL} target="_blank" rel="noopener noreferrer">
+            GH
+        </a>
+    );
+}
+
+export default GHLink;
